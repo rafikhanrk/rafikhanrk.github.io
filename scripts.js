52859d4cc20bec86b8c0cb37f0bf8f03537850bf
rafikhanrk <bythakhan@gmail.com>
2024-07-18 23:34:22 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -95,23 +95,4 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
-
-    // Scroll-triggered transitions
-    const sections = document.querySelectorAll('.section');
-    const observerOptions = {
-        threshold: 0.1
-    };
-
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('visible');
-                observer.unobserve(entry.target);
-            }
-        });
-    }, observerOptions);
-
-    sections.forEach(section => {
-        observer.observe(section);
-    });
 });
